133732107500598b721da11abf712b768930c348
Jorge <jorge.lrs497@gmail.com>
2019-10-12 23:01:13 -0500

    insercion de datos

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -7,6 +7,23 @@ router.get('/', async (req, res) =>{
     res.send(await posts.find({}).toArray())
 })
 
+//Add posts
+router.post('/', async(req, res)=>{
+    const posts = await loadPosts()
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    })
+    res.status(201).send()
+})
+
+//Delete post
+router.delete('/:id', async(req, res)=>{
+    const posts = await loadPosts()
+    await posts.deleteOne({id:new mongodb.ObjectID(req.params.id)})
+    res.status(200).send()
+})
+
 async function loadPosts(){
     const client = await mongodb.MongoClient.connect
     ('mongodb+srv://Guest:foro2019@cluster0-jsdvz.mongodb.net/test?retryWrites=true&w=majority', {
